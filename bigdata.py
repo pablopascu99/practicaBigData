63ba3a99815bc6c31f0ed3f381c9b2f2f11fb2f0
pablopascu99 <59370966+pablopascu99@users.noreply.github.com>
2021-11-17 18:47:56 +0100

    Add files via upload

--- bigdata.py
+++ bigdata.py
@@ -5,11 +5,27 @@ import pandas as pd
 sc = SparkContext('local')
 spark = SparkSession(sc)
 
-d=(spark.read.csv('./PracticaBigData/datos/cards.csv',header=True,inferSchema=True,sep = '|',)).toPandas()
-d = d.withColumn('IMPORTE', regexp_replace('IMPORTE', ',', '.'))
-#print(d.dtypes)
-#i=d["IMPORTE"].astype(float, errors="raise")
-#i=d["IMPORTE"].replace(',','.')
-#g=i.groupby(by = "SECTOR")["IMPORTE"].mean()
-print(i)
-#print(g)
+#Importe medio por sector
+
+'''df1 = spark.read.csv("./PracticaBigData/datos/cards.csv",header=True,inferSchema=True,sep = '|',)
+p = df1.toPandas()
+p['IMPORTE'] = [x.replace(',', '.') for x in p['IMPORTE']]
+p['IMPORTE'] = p['IMPORTE'].astype(float)
+p.to_csv('./PracticaBigData/datos/cards1.csv')
+df = spark.read.csv("./PracticaBigData/datos/cards1.csv",header=True,inferSchema=True,sep = ',',)
+importeMedioSector = df.groupBy("SECTOR").mean( "IMPORTE") 
+importeMedioSector.show()'''
+
+#Codigos Postales
+
+df2 = spark.read.csv("./PracticaBigData/datos/codigoPostalCoordenadas.csv",header=True,inferSchema=True,sep = ';')
+df2.printSchema()
+
+df = spark.read.csv("./PracticaBigData/datos/cards1.csv",header=True,inferSchema=True,sep = ',')
+importeCP = df.groupBy("CP_CLIENTE").sum("IMPORTE") 
+importeCP.printSchema()
+'''importeCP.join(df2, importeCP("CP_CLIENTE") === df2("codigopostalid"))
+importeCP.show()'''
+i=importeCP.join(df2,importeCP.CP_CLIENTE == df2.codigopostalid,"left")
+i.show()
+i.dropDuplicates(['CP_CLIENTE','codigopostalid']).show()
