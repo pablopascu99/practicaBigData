64dcae637c233aacae81cfb988ae76ac32df8cd8
pablopascu99 <pablopascu99@gmail.com>
2021-11-19 12:05:50 +0100

    j

---
+++ backend.py
@@ -0,0 +1,45 @@
+from pyspark.context import SparkContext
+from pyspark.sql.session import SparkSession
+import pandas as pd
+from google.cloud import storage
+
+sc = SparkContext('local')
+spark = SparkSession(sc)
+
+#Importe medio por sector
+
+df1 = spark.read.csv("./datos/cards.csv",header=True,inferSchema=True,sep = '|')
+data = df1.toPandas()
+data['IMPORTE'] = [x.replace(',', '.') for x in data['IMPORTE']]
+data['IMPORTE'] = data['IMPORTE'].astype(float)
+data.to_csv('./datos/cardsNuevo.csv')
+
+df = spark.read.csv("./datos/cardsNuevo.csv",header=True,inferSchema=True,sep = ',')
+importeMedioSector = df.groupBy("SECTOR").mean("IMPORTE")
+importeMedioSector.toPandas().to_csv('./datos/importeMedioSector.csv')
+
+#Codigos Postales
+
+df2 = spark.read.csv("./datos/codigoPostalCoordenadas.csv",header=True,inferSchema=True,sep = ';')
+df3 = spark.read.csv("./datos/weather.csv",header=True,inferSchema=True,sep = ';')
+
+importeCP = df.groupBy("CP_CLIENTE").sum("IMPORTE") 
+dfImporte = importeCP.join(df2,importeCP.CP_CLIENTE == df2.codigopostalid,"left")
+dfImporte.dropDuplicates(['CP_CLIENTE','codigopostalid']).drop("CP_CLIENTE")
+dfImporte.toPandas().dropna().to_csv('./datos/importePorLocalidad.csv')
+dfx = spark.read.csv("./datos/importePorLocalidad.csv",header=True,inferSchema=True,sep = ',')
+
+#Franja horaria con mas ventas
+
+i=df.groupBy("FRANJA_HORARIA").sum("NUM_OP")
+j=df.groupBy("FRANJA_HORARIA").sum("IMPORTE")
+k=i.join(j,i.FRANJA_HORARIA == j.FRANJA_HORARIA, "left")
+k.show(20)
+
+#Franja horaria con mas ventas
+'''m1=df[(df["SECTOR"]=="OCIO Y TIEMPO LIBRE")]
+m=m1.groupBy("DIA").sum("NUM_OP") 
+k=df3.join(m,df3.FECHA == m.DIA, "left").select(m["DIA"],df3["Precip"],m["sum(NUM_OP)"])
+k.groupBy("DIA").sum("sum(NUM_OP)")
+k1=k[(k["Precip"]>=5)]
+k1.show(30)'''
---
+++ frontend.py
@@ -0,0 +1,38 @@
+import numpy as np
+import streamlit as st
+import pandas as pd
+from google.cloud import storage
+import datetime
+from enum import Enum
+import os
+import json
+from streamlit_folium import folium_static
+import folium
+
+df = pd.read_csv('./datos/importePorLocalidad.csv',sep = ',')
+df.drop(df.columns[[0]], axis=1, inplace=True)
+df['CP_CLIENTE'] = df['CP_CLIENTE'].apply(lambda x: '{0:0>5}'.format(x))
+
+with open('./datos/almeria_20.json') as f:
+  states_topo = json.load(f)
+
+st.write(states_topo['objects']['almeria_wm']['geometries'][0]['properties']['COD_POSTAL'])
+
+m = folium.Map(location=[37.16, -2.33], tiles='Stamen Terrain', zoom_start=9)
+
+folium.Choropleth(
+    geo_data=states_topo,
+    topojson='objects.almeria_wm',
+    name="choropleth",
+    line_color='white',
+    line_weight=1,
+    data=df,
+    columns=["CP_CLIENTE", "sum(IMPORTE)"],
+    key_on="feature.properties.COD_POSTAL",
+    fill_color="YlOrRd",
+    fill_opacity=0.6,
+    line_opacity=0.4,
+    legend_name="Importe total que gasta cada localidad",
+).add_to(m)
+
+folium_static(m)
