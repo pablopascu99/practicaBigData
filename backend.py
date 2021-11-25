85430b891c163c42db13e06fab2dc739c1742025
joseIgnaciodv <joseignaciodelvallebustillo@gmail.com>
2021-11-25 15:20:24 +0100

    Cambios para lectura del bucket de Google

--- backend.py
+++ backend.py
@@ -8,33 +8,37 @@ spark = SparkSession(sc)
 
 #Importe medio por sector
 
-df1 = spark.read.csv("./datos/cards.csv",header=True,inferSchema=True,sep = '|')
-data = df1.toPandas()
+data = pd.read_csv('gs://bucket-prueba-nacho/cards.csv', storage_options=({"token":"datos\grandes-volumenes-9d18b4ccbb2f.json"}), sep= '|')
 data['IMPORTE'] = [x.replace(',', '.') for x in data['IMPORTE']]
 data['IMPORTE'] = data['IMPORTE'].astype(float)
 data.to_csv('gs://bucket-prueba-nacho/cardsNuevo.csv', storage_options=({"token":"datos\grandes-volumenes-9d18b4ccbb2f.json"}))
 
-df = spark.read.csv("./datos/cardsNuevo.csv",header=True,inferSchema=True,sep = ',')
+
+df = pd.read_csv("gs://bucket-prueba-nacho/cardsNuevo.csv", storage_options=({"token":"datos\grandes-volumenes-9d18b4ccbb2f.json"}), sep = ',')
+df = spark.createDataFrame(df)
+df.show()
 importeMedioSector = df.groupBy("SECTOR").mean("IMPORTE")
 importeMedioSector.toPandas().to_csv('gs://bucket-prueba-nacho/importeMedioSector.csv', storage_options=({"token":"datos\grandes-volumenes-9d18b4ccbb2f.json"}))
 
 #Codigos Postales
 
-df2 = spark.read.csv("./datos/codigoPostalCoordenadas.csv",header=True,inferSchema=True,sep = ';')
-df3 = spark.read.csv("./datos/weather.csv",header=True,inferSchema=True,sep = ';')
+df2 = pd.read_csv("gs://bucket-prueba-nacho/codigoPostalCoordenadas.csv",sep = ';', storage_options=({"token":"datos\grandes-volumenes-9d18b4ccbb2f.json"}))
+df3 = pd.read_csv("gs://bucket-prueba-nacho/weather.csv",sep = ';', storage_options=({"token":"datos\grandes-volumenes-9d18b4ccbb2f.json"}))
+
+df2 = spark.createDataFrame(df2)
+df3 = spark.createDataFrame(df3)
 
 importeCP = df.groupBy("CP_CLIENTE").sum("IMPORTE") 
 dfImporte = importeCP.join(df2,importeCP.CP_CLIENTE == df2.codigopostalid,"left")
 dfImporte.dropDuplicates(['CP_CLIENTE','codigopostalid']).drop("CP_CLIENTE")
 dfImporte.toPandas().dropna().to_csv('gs://bucket-prueba-nacho/importePorLocalidad.csv', storage_options=({"token":"datos\grandes-volumenes-9d18b4ccbb2f.json"}))
-dfx = spark.read.csv("./datos/importePorLocalidad.csv",header=True,inferSchema=True,sep = ',')
 
 #Franja horaria con mas ventas
 
 i=df.groupBy("FRANJA_HORARIA").sum("NUM_OP")
 j=df.groupBy("FRANJA_HORARIA").sum("IMPORTE")
 k=i.join(j,i.FRANJA_HORARIA == j.FRANJA_HORARIA, "left").drop(i.FRANJA_HORARIA)
-k.show(20)
+k.toPandas().to_csv('gs://bucket-prueba-nacho/franjaHorariaVentas.csv', storage_options=({"token":"datos\grandes-volumenes-9d18b4ccbb2f.json"}))
 
 #Franja horaria con mas ventas
 '''m1=df[(df["SECTOR"]=="OCIO Y TIEMPO LIBRE")]
